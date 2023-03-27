39f6cfbbba7e0d0eb780243fc08c1f6b44d13902
btorpey00 <120498601+btorpey00@users.noreply.github.com>
2023-03-27 16:42:03 -0400

    set up initial route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import About from './pages/About';
 
 import './App.css'
 
 function App() {
 
 
-  return (
-    <div className="App">
 
-    </div>
+  
+  useEffect(() => {
+  async function getProducts() {
+    try {
+      const response = await fetch('https://dummyjson.com/products');
+      const productData = await response.json();
+      console.log(productData)
+
+    } catch(error) {
+      console.log(error)
+    }
+  }
+  getProducts()
+},[])
+
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <div className="App">
+        <Routes>
+          <Route path='/shopping-cart' element={<Home />} />
+          <Route path='/shopping-cart/shop' element={<Shop />} />
+          <Route path='/shopping-cart/about' element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div className="nav-bar">
+            <ul className="nav-link-list">
+                <li className="nav-link"><Link to='/shopping-cart'>Home</Link></li>
+                <li className="nav-link"><Link to='/shopping-cart/shop'>Shop</Link></li>
+                <li className="nav-link"><Link to='/shopping-cart/about'>About</Link></li>
+            </ul>
+            <div className="cart-icon-container">
+                <FontAwesomeIcon icon={faCartShopping} className="cart-icon" />
+                <div className="items-in-cart">22</div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
