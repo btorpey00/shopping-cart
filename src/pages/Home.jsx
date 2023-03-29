171c8c89f8c0fb6d4e5b3c00a8daea9ddb286435
btorpey00 <120498601+btorpey00@users.noreply.github.com>
2023-03-29 18:22:50 -0400

    add some content to Home page and edit some styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import './Home.css';
+
 export default function Home() {
     
     return (
-        <div>
-            Home
+        <div className="home-page-container">
+            <div className="home-page-content">
+                <div className="left-side">
+                    <Link to='/shopping-cart/shop' ><button className="home-shopping-button">Start Shopping Now!</button></Link>
+                </div>
+                <div className="right-side">
+                    Welcome to the greatest fake tech store there is! We offer something for everybody.  The best part is you won't even spend any real money (although you won't receive any products either)  Start fake shopping today!
+                </div>
+            </div>
         </div>
 
     )
